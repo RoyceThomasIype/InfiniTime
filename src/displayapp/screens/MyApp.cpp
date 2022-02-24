e702e0788a4edbde3175ef8f5600869af75cfda1
Royce Thomas Iype <39577739+RoyceThomasIype@users.noreply.github.com>
2022-02-24 20:31:26 +0100

    Update MyApp.cpp

--- src/displayapp/screens/MyApp.cpp
+++ src/displayapp/screens/MyApp.cpp
@@ -5,7 +5,7 @@ using namespace Pinetime::Applications::Screens;
 
 MyApp::MyApp(DisplayApp* app) : Screen(app) {
   lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "My test application");
+  lv_label_set_text_static(title, "Test Application");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
 }
