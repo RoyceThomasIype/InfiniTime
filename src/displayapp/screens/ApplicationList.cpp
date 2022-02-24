6489521d7a6abdafcff5c73c2656c5710cd28a77
Royce Thomas Iype <39577739+RoyceThomasIype@users.noreply.github.com>
2022-02-24 20:12:26 +0100

    Update ApplicationList.cpp

--- src/displayapp/screens/ApplicationList.cpp
+++ src/displayapp/screens/ApplicationList.cpp
@@ -64,9 +64,9 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
   return std::make_unique<Screens::Tile>(1, 2, app, settingsController, batteryController, dateTimeController, applications);
 }
 
-/*std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
+std::unique_ptr<Screen> ApplicationList::CreateScreen3() {
   std::array<Screens::Tile::Applications, 6> applications {
-          {{"A", Apps::Meter},
+          {{"A", Apps::MyApp},
            {"B", Apps::Navigation},
            {"C", Apps::Clock},
            {"D", Apps::Music},
@@ -76,4 +76,4 @@ std::unique_ptr<Screen> ApplicationList::CreateScreen2() {
   };
 
   return std::make_unique<Screens::Tile>(2, 3, app, settingsController, batteryController, dateTimeController, applications);
-}*/
+}
